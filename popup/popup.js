86088352e0515094a33cfba628de9d20aaa99840
chococorone <omochi.nokinako@gmail.com>
2023-01-15 12:36:56 +0900

    implement delete button

--- popup/popup.js
+++ popup/popup.js
@@ -4,24 +4,44 @@ async function getDb() {
     return hashTags;
 }
 
-console.log('choco');
+function updatePopup() {
+    const list = document.getElementById('hashtag_list');
 
-const list = document.getElementById('hashtag_list');
+    getDb().then((result) => {
+        const keys = Object.keys(result);
 
-getDb().then((result) => {
-    const keys = Object.keys(result);
+        keys.forEach((key) => {
+            let a_element = document.createElement('a');
+            a_element.href = result[key];
+            a_element.target = '_blank';
+            a_element.rel = 'noopener';
+            a_element.textContent = '#' + key;
+            a_element.classList.add('tag');
+            console.log(a_element);
 
-    keys.forEach((key) => {
-        let p_element = document.createElement('p');
-        let a_element = document.createElement('a');
-        a_element.href = result[key];
-        a_element.target = '_blank';
-        a_element.rel = 'noopener';
-        a_element.textContent = '#' + key;
-        console.log(a_element);
-        p_element.appendChild(a_element);
-        p_element.classList.add('tags');
-        console.log(p_element);
-        list.appendChild(p_element);
+            let button_element = document.createElement('button');
+            button_element.classList.add('close');
+            button_element.onclick = async () => {
+                await removeTag(key);
+                p_element.removeChild(a_element);
+                p_element.removeChild(button_element);
+            };
+
+            let p_element = document.createElement('p');
+            p_element.appendChild(a_element);
+            p_element.appendChild(button_element);
+            p_element.classList.add('tags');
+            console.log(p_element);
+
+            list.appendChild(p_element);
+        });
     });
-});
+}
+
+async function removeTag(key) {
+    console.log('removeTag');
+    await chrome.storage.local.remove(key);
+}
+
+console.log('choco');
+updatePopup();
